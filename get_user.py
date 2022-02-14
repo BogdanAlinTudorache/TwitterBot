ccbd27c4075557ff34cb47a1e7a5fb9e7e5a43c4
Bogdan Tudorache <tudorache.a.bogdan@gmail.com>
2022-02-14 21:17:54 +0100

    Update get_user.py

--- get_user.py
+++ get_user.py
@@ -2,14 +2,12 @@ import tweepy
 import time
 import sys
 
-# NOTE: I put my keys in the keys.py to separate them
+# NOTE: I put my keys in the credentials.py to separate them
 # from this main file.
-# Please refer to keys_format.py to see the format.
+# Please refer to credentials.py to see the format.
 from credentials import twitter_bot_keys
 
 # NOTE: flush=True is just for running this script
-# with PythonAnywhere's always-on task.
-# More info: https://help.pythonanywhere.com/pages/AlwaysOnTasks/
 print('\nThis is my twitter bot: BerryNews Bot - BNB.\nStarted running...', flush=True)
 
 auth = tweepy.OAuthHandler(twitter_bot_keys['API Key'], twitter_bot_keys['API Key Secret'])
@@ -23,23 +21,7 @@ try:
 except:
     print("Error during authentication\n")
  
-## POSTING TWEET
-nl_en_link = "http://berrynews.org/netherlands-en.html"
-ro_en_link = "http://berrynews.org/romania-en.html"
-ro_ro_link = "http://berrynews.org/romania-ro.html"
-text="Gazele s-au scumpit din cauza rușilor,spune ministrul Energiei"
-tweet = "BNB:\n"+text+"\n"+ ro_ro_link +"\n#twitterbot #stiri #romania #berrynews"  
-
-tweet = '@b_tudorache#Hello from the dark side!'
-# if len(tweet) <160:
-#     print('Posting tweet:', tweet,'\n')
-#     api.update_status(tweet, in_reply_to_status_id=  1469339850851729412)
-# else:
-#     print('Tweet is bigger than 150 characers, stopping...')
-#     print(len(tweet))
-#     sys.exit()
-
-## NOT WORKING
+## GET USER BY NAME
 user = api.get_user(screen_name="berrynewsorg")
 print("User details:")
 print('id:',user.id)
